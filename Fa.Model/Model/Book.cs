4d038ee8106aa9adfef701b5fdd2f4a48007a12a
toiye <toiye@DESKTOP-BJB9GPG>
2018-08-03 10:19:17 +0700

    Book Class

--- Fa.Model/Model/Book.cs
+++ Fa.Model/Model/Book.cs
@@ -1,7 +1,43 @@
-﻿namespace Fa.Model.Model
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fa.Model.Model
 {
     public class Book
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookId { get; set; }
 
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        public string Summary { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public  virtual Author Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quality { get; set; }
+
+        public DateTime DatePublisher { get; set; }
+
+        [MaxLength(500)]
+        public string ImageUrl { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
     }
 }
--- Fa.Model/Model/Publisher.cs
+++ Fa.Model/Model/Publisher.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Fa.Model.Model
 {
     public class Publisher
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PublisherId { get; set; }
 
         [MaxLength(50)]
