b1031789808900fca13114dad38f241f2ee6f846
toiye <toiye@DESKTOP-BJB9GPG>
2018-08-03 10:33:13 +0700

    DataAccess: Chuyển hóa model thành cỏ sở dữ liệu

--- Fa.DataAccessLayer/Interface/ICategoryRepository.cs
+++ Fa.DataAccessLayer/Interface/ICategoryRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fa.Model.Model;
 
 namespace Fa.DataAccessLayer.Interface
 {
     public interface ICategoryRepository : IDisposable
     {
+        IEnumerable<Category> GetCategories();
+        Category GetCategoryId(int categoryId);
+        int Insert(Category category);
+        int Update(Category category);
+        int Delete(int categoryId);
     }
 }
---
+++ Fa.DataAccessLayer/ManagermentContext/BookManagermentContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fa.Model.Model;
+
+namespace Fa.DataAccessLayer.ManagermentContext
+{
+    class BookManagermentContext : DbContext
+    {
+        public BookManagermentContext():base("name=BookConnectionString")
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookInPublisher> BookInPublishers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
