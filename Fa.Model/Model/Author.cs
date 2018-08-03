b7c59bc31083ed597791c0fb80cd78c019448754
toiye <toiye@DESKTOP-BJB9GPG>
2018-08-03 10:10:03 +0700

    Author Class

--- Fa.Model/Model/Author.cs
+++ Fa.Model/Model/Author.cs
@@ -1,7 +1,18 @@
-﻿namespace Fa.Model.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Fa.Model.Model
 {
     public class Author
     {
+        [Key]
+        public int AuthorId { get; set; }
 
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string History { get; set; }
+
+        public virtual  IEnumerable<Book> Books { get; set; }
     }
 }
